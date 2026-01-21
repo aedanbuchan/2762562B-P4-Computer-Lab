0f0d3e1e4afa864464085ef4438d91a4889a0e2f
aedanbuchan <116572853+aedanbuchan@users.noreply.github.com>
2026-01-21 16:49:55 +0000

    Update __init__.py
    updating init.py file to include new fitting functions for perodic functions

--- code_functions/__init__.py
+++ code_functions/__init__.py
@@ -1,3 +1,6 @@
 from code_functions.math import add
 from code_functions.fitting import linear
 from code_functions.fitting import linear_fit_plot
+from code_functions.fitting import sin
+from code_functions.fitting import periodic_fit_plot
+from code_functions.fitting import linear_fit_plot_whole
