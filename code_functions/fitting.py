7782ae733ca068c7380a66256a0e62ec86df940b
aedanbuchan <116572853+aedanbuchan@users.noreply.github.com>
2026-01-22 23:37:59 +0000

    Update fitting.py
    Added proper documentation for each function and small changes to periodic whole function

--- code_functions/fitting.py
+++ code_functions/fitting.py
@@ -1,15 +1,54 @@
 import numpy as np
-from scipy import optimize  # <-- this is needed for optimize.curve_fit
+from scipy import optimize
 import matplotlib.pyplot as plt
 
-
-## Defining linear function for curve fitting
 def linear(x, m, c):
+    """
+       Simple linear function for fitting purposes
+
+        Parameters
+        ----------
+        x : array
+            x axis data for the straight line
+        m : number
+            Gradient of straight line
+        c : number
+            Y -intercept of straight line
+
+        Returns
+        -------
+        m*x + c : An array of the y values related to the x-data by the straight line equatio defined by the given
+        gradient and y-intercept
+        """
     return m*x + c
 
 ## Definig linear plot function to complete all tasks from excercise 1 for any give x and y data set
 def linear_fit_plot(x_variable, y_variable):
+    """
+      Uses optimize.curvefit to fit and plot a linear function to raw x and y data.
 
+        Parameters
+        ----------
+        x_variable : array
+            x axis data
+        y_variable : array
+            y_axis data
+        
+        Returns
+        -------
+        Plots including raw data, fitted linear function and residuals.
+        
+        m : number
+            Fitted parameter defining the gradient of the straight line found by curve.fit
+        merr : number
+            Caculated error on gradient parameter
+        c : number
+            Fitted parameter defining the y-intercept of the straight line found by curve.fit
+        cerr : number
+            Caculated error on y-intercept parameter
+        r_squared :
+            Calculated r squared value to asses goodness of fit
+    """
 ## Using optimize.curve_fit to fit a linear funtion to our x and y data
     params_linear, params_linear_covariance = optimize.curve_fit(linear,x_variable,y_variable)
 
@@ -62,9 +101,60 @@ def linear_fit_plot(x_variable, y_variable):
     return(m,merr,c,cerr,r_squared)
 
 def sin(x,a,b,c,d,e,f,g):
+     """
+       Perodic function including a sine and cosine term for fitting purposes
+
+        Parameters
+        ----------
+        x : array
+            x axis data for the perodic function
+        a : number
+            Amplitude of sine term
+        b : number
+            Term defing period of sine term
+        c : number
+            Term defing angle shift of sine term
+        d : number
+            Term defing up or down shift of both terms
+        e : number
+            Amplitude of cosine term
+        f : number
+            Term defing period of cosine term
+        g : number
+            Term defing angle shift of cosine term
+        
+        Returns
+        -------
+        a*np.sin(b*x + c) + d + e*np.cos(f*x + g) : Array
+            An array of the y values related to the x-data by the perodic function defined by the calculated
+            parameters
+        """
     return a*np.sin(b*np.deg2rad(x) + c) + d + e*np.cos(f*np.deg2rad(x) + g)
 
 def periodic_fit_plot(x_variable, y_variable,initial, plot = False):
+    """
+      Uses optimize.curvefit to fit and plot a perodic function to raw x and y data.
+
+        Parameters
+        ----------
+        x_variable : array
+            x axis data
+        y_variable : array
+            y_axis data
+        intial : array
+            Array of intial guesses of parameters
+        plot : Boolean
+            Parameter defining if data is plotted or not
+        
+        Returns
+        -------
+        Plots including raw data, fitted perodic function and residuals.
+        
+        params_sin : array
+            Fitted parameters found by curve.fit
+        errs : array
+            Caculated errors on parameters
+    """
 ## Using optimize.curve_fit to fit a linear funtion to our x and y data
     params_sin, params_sin_covariance = optimize.curve_fit(sin,x_variable,y_variable,p0=initial,maxfev=10000)
 
@@ -114,29 +204,60 @@ def periodic_fit_plot(x_variable, y_variable,initial, plot = False):
 
     return(params_sin,errs)
 
-def periodic_fit_plot_whole(dataset,initial, plot = False):
+## Defining a function to iterate previous function over the whole data set and plot the parameters for each slice 
 
+def periodic_fit_plot_whole(dataset,initial, plot = False):
+    """
+      Uses peordic fitting function to iterate over whole data set, calculates and plots parameters for each slice
+
+        Parameters
+        ----------
+        dataset : array
+            3d array of intensity data
+        inital : array
+            array of inital parameters guess
+        plot : Boolean
+            Parameter defining if data is plotted or not
+        
+        Returns
+        -------
+        Plots of each parameter with assosiated errors.
+        
+        params_array : array
+            array of arrays of calulated parameters for each slice
+
+        errors_array : array
+            array of arrays of calulated errors for each slice
+    """
     import matplotlib.pyplot as plt
     import matplotlib.gridspec as gridspec
 
+## Opening empty arrays to fill with parameters and associated errors
     params_array = []
     errors_array = []
-    for x in range(len(dataset[:,0,0])):
-        for y in range(len(dataset[0,:,0])):
-            params = periodic_fit_plot(degrees, dataset[x,y,:],initial, plot = False)
+
+## Beginning for loop to iterate over every slice of data
+    for x in range(len(dataset[:,0,0])): # This begins loop for length of one dimension
+        for y in range(len(dataset[0,:,0])): # For every value in first dimension we iterate over all slices in the 2nd
+            params = periodic_fit_plot(degrees, dataset[x,y,:],initial, plot = False) # Runs previous function on each slice
             params_array.append(params[0])
             errors_array.append(params[1])
             #print(x,y)
+
+## Opens empty array to store parameters and errors
     parameters = [[] ,[],[], [], [], [], []]
     parameters_err = [[] ,[],[], [], [], [], []]
-
+## Linspace for plotting parameters
     dummy_parameters = np.linspace(0,len(dataset[0])**2,len(dataset[0])**2)
 
+## Sorts parameters into arrays within larger array
     for y in range(7):
-        for x in range(len(params_whole[0])):
-            parameters[y].append(params_whole[0][x][y])
-            parameters_err[y].append(params_whole[1][x][y])
-    fig = plt.figure(figsize=(14, 6))
+        for x in range(len(params_array)):
+            parameters[y].append(params_array[x][y])
+            parameters_err[y].append(errors_array[x][y])
+    
+## Plots each parameter for each slice and assosiated errors
+    fig = plt.figure(figsize=(14, 10))
     gs = gridspec.GridSpec(2, 8)
 
     axes = []
@@ -148,12 +269,12 @@ def periodic_fit_plot_whole(dataset,initial, plot = False):
     for i in range(3):
         axes.append(fig.add_subplot(gs[1, 1 + i*2 : 1 + (i+1)*2]))
 
-# Plot different data on each subplot
+# Plot different parameters and their errors on each subplot
     for z in range(7):
         axes[z].set_title("Parameter " + str(z+1))
         axes[z].errorbar(dummy_parameters,parameters[z],yerr=parameters_err[z], fmt='o',label="Data Points",color="black")
-
-
+        axes[z].plot([], [], ' ', label="Mean = {0:.4f}".format(np.mean(parameters[z]))) ## displaying r squared value
+        axes[z].legend()
     plt.tight_layout()
     plt.show()
     return params_array, errors_array
