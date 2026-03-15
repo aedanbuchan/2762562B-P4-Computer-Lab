2f5708e4427eb852f649fcae20323a22ee07e8e4
aedanbuchan <116572853+aedanbuchan@users.noreply.github.com>
2026-03-15 22:59:49 +0000

    Update __init__.py
    updated init file to reflect new repo structure (not yet applied but moving towards this structure

--- PhysComp/__init__.py
+++ PhysComp/__init__.py
@@ -1,3 +1,5 @@
-import PhysComp.fitting
-import PhysComp.Sim
-import PhysComp.UML
+from PhysComp.fitting import *
+from PhysComp.sim import *
+from PhysComp.uml import *
+from PhysComp.assess import *
+from PhysComp.visual import *
